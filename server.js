185bc922b7fabbdf523f21d5150815094cc66e0c
jelle-noordzij-pixel <jelle-noordzij@hotmail.com>
2026-05-12 16:38:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,35 +26,24 @@ function buildDeck() {
 }
 
 io.on("connection", (socket) => {
-    console.log("Connectie:", socket.id);
-
     socket.on("joinGame", (name) => {
-        gameState.players[socket.id] = {
-            id: socket.id,
-            name: name || "Speler",
-            hand: [],
-            score: 0
-        };
-        if (!gameState.playerOrder.includes(socket.id)) {
-            gameState.playerOrder.push(socket.id);
-        }
+        gameState.players[socket.id] = { id: socket.id, name: name || "Speler", hand: [], score: 0 };
+        if (!gameState.playerOrder.includes(socket.id)) gameState.playerOrder.push(socket.id);
         io.emit("updateState", gameState);
     });
 
-    socket.on("startGame", () => {
-        console.log("SERVER: Starten van het spel...");
+    // We noemen de actie nu simpelweg 'GO' om verwarring te voorkomen
+    socket.on("GO", () => {
+        console.log("START SIGNAAL ONTVANGEN!"); 
         gameState.status = 'PLAYING';
         gameState.deck = buildDeck();
         gameState.tableStack = [gameState.deck.pop()];
         
         gameState.playerOrder.forEach(id => {
-            if (gameState.players[id]) {
-                gameState.players[id].hand = gameState.deck.splice(0, 5);
-            }
+            if (gameState.players[id]) gameState.players[id].hand = gameState.deck.splice(0, 5);
         });
         
         gameState.turnIndex = 0;
-        // We sturen de VOLLEDIGE state naar IEDEREEN (geen geheimhouding nu, eerst testen of het werkt)
         io.emit("updateState", gameState);
     });
 
@@ -66,4 +55,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server draait`));
+server.listen(PORT, () => console.log("Server is gestart"));
